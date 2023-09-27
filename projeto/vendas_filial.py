d784c312e29598c35435c8a1ca827755bc892071
Marcelo Porfirio <79551082+MarceloPorfirio@users.noreply.github.com>
2023-09-26 23:15:36 -0300

    Update vendas_filial.py

--- projeto/vendas_filial.py
+++ projeto/vendas_filial.py
@@ -1,7 +1,6 @@
 # from matplotlib.pyplot import subplots
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 import calendar
 import plotly.express as px
 import locale  # Importe o módulo locale
